f54aad822b1c1e4db39eb18e9015555f5a7f2e7d
Josh Rowe <josh@s-block.com>
2016-10-13 17:23:55 +0100

    add postgres database config and requirements

--- fee_calculator/settings/base.py
+++ fee_calculator/settings/base.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'fee_calculator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'fee_calculator'),
+        'USER': os.environ.get('DB_USERNAME', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
 
