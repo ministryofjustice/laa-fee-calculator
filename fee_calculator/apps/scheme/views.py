61a97d59b92965a2b80ebe7d3ade59dbe109d8f0
Josh Rowe <josh@s-block.com>
2016-10-20 15:32:29 +0100

    add comments to api views

--- fee_calculator/apps/scheme/views.py
+++ fee_calculator/apps/scheme/views.py
@@ -22,10 +22,25 @@ class SchemeViewSetMixin():
 
 
 class BaseSchemeViewSet(SchemeViewSetMixin, viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows schemas to be viewed.
+
+    retrieve:
+    Return a user instance.
+
+    list:
+    Return all schemes, ordered by id
+    """
     lookup_value_regex = '\d+'
 
 
 class SchemeViewSet(SchemeViewSetMixin, viewsets.GenericViewSet):
+    """
+    API endpoint that returns single scheme for suty type for a given case
+    start date in format YYYY-MM-DD
+
+    /api/v1/scheme/advocate/2016-12-07/
+    """
     lookup_field = 'suty'
     lookup_value_regex = '|'.join([t.lower() for t in SUTY_BASE_TYPE.constants])
 
