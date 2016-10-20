1cf07888e3f5d9466fa27eda3e92322bd28f3383
Josh Rowe <josh@s-block.com>
2016-10-20 11:57:33 +0100

    update swagger import

--- fee_calculator/apps/calculator/urls.py
+++ fee_calculator/apps/calculator/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
 
-from rest_framework_swagger.views import get_swagger_view
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 from scheme.views import BaseSchemeViewSet, SchemeViewSet
 
@@ -15,5 +15,5 @@ schema_view = get_swagger_view(title='Calculator API')
 
 urlpatterns = (
     url(r'^', include(router.urls)),
-    url(r'^docs/', schema_view)
+    url(r'^docs/$', schema_view)
 )
