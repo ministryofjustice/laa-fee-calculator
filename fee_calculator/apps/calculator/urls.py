fa130cfd7698268174a398ec1d5e1b4bbc5c89fe
Josh Rowe <josh@s-block.com>
2016-10-20 10:25:14 +0100

    add swagger view

--- fee_calculator/apps/calculator/urls.py
+++ fee_calculator/apps/calculator/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
 
+from rest_framework_swagger.views import get_swagger_view
 from rest_framework import routers
 
 from scheme.views import BaseSchemeViewSet, SchemeViewSet
@@ -10,6 +11,9 @@ router = routers.DefaultRouter()
 router.register(r'scheme', BaseSchemeViewSet, base_name='scheme')
 router.register(r'scheme', SchemeViewSet, base_name='scheme-by-date')
 
+schema_view = get_swagger_view(title='Calculator API')
+
 urlpatterns = (
     url(r'^', include(router.urls)),
+    url(r'^docs/', schema_view)
 )
