0a5a67c7d423441e3545095df624b8b38d3f4a9a
Cliff Xuan <cliff.xuan@gmail.com>
2016-10-28 15:01:12 +0100

    removed a comment

--- fee_calculator/apps/calculator/filters.py
+++ fee_calculator/apps/calculator/filters.py
@@ -11,10 +11,6 @@ def swagger_filter_backend_class(api_doc):
     class SwaggerFilterBackend(DjangoFilterBackend):
 
         def get_schema_fields(self, view):
-            # Field definition
-            # Field = namedtuple(
-            # 'Field', ['name', 'required', 'location', 'type', 'description']
-            # )
             keys = ('name', 'required', 'location', 'type', 'description')
             return [coreapi.Field(**{name: value for name, value
                                      in field.items() if name in keys})
