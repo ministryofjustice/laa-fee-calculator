cd6473bf25427c9a0a9cc894bf974c8639ba4e34
ale-novo <36439993+ale-novo@users.noreply.github.com>
2018-04-09 11:35:04 +0100

    Feature/k8s deployment (#29)
    * jason asked
    
    * bump
    
    * Created blank deployment files for k8s integration
    
    * bump
    
    * llala
    
    * bump
    
    * sorry
    
    * bump
    
    * Padded out k8s deployment info
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * password generation and s3 upload
    
    * fix
    
    * bump
    
    * added policy
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * K8 deploy files
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * Added namespaces and new ECR link
    
    * bump
    
    * bump
    
    * Changed the TAG value for ECR
    
    * Added a deploy delete before apply
    
    * Added wait condition to buildspec
    
    * Removed the delete from buildspec
    
    * Changed grep command in buildspec
    
    * Added env to manage.py command in buildspec
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * bump
    
    * Decrease size of the output docker image
    
    1/ Use Alpine Linux image
    2/ Install only necessary packages
    
    It is possible to improve it even further down the line by copying packages which
    were built during the step 1 and avoiding installation of all the buildtools / gcc
    
    * Second step of image size reduction
    
    Removed obsolete packages installed during the first build step and moved artifacts
    to the newly created docker image.
    
    * bump
    
    * Add bash installation for the final docker image.
    
    Bash shell is required by scripts which need to be run after new image creation
    
    * Hotfix: Copy over whole python directory.
    
    It should get rid of the errors with missing packages which were installed
    in different locations than site-packages.
    
    * Import data on the docker image run
    
    To avoid issues with kubernetes cluster executing commands on live image
    we can deploy an image and import the data during the startup.
    As it takes a while website won't be available during this process.
    
    * removed python population script that was added to the dockerfile
    
    * bump
    
    * reverted dockerfile
    
    * added db migration script run

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "laa-fee-calculator"
+}
+
+variable "git_repo" {
+  default = "laa-fee-calculator"
+}
+
+variable "git_branch" {
+  default = "feature/k8s-deployment"
+}
+
+variable "git_token" {
+  default = "6b9f603904a73ce45bfe8f3bab09a52ccf4b3d0e"
+}
