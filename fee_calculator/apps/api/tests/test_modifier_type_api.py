be0dbc11a8f18f148c87847222ae8316e2ee5396
Ian Brechin <ian.brechin@gmail.com>
2018-06-27 12:48:25 +0100

    Correctly report invalid fee_type_code in error

--- fee_calculator/apps/api/tests/test_modifier_type_api.py
+++ fee_calculator/apps/api/tests/test_modifier_type_api.py
@@ -23,7 +23,7 @@ class ModifierTypeApiTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertGreater(len(response.data['results']), 0)
 
-    def test_get_list_400_with_invalid_filter(self):
+    def test_get_list_400_with_invalid_scenario(self):
         response = self.client.get(
             self.endpoint.format(scheme=1) +
             '?offence_class=A&advocate_type=JRALONE&scenario=burps&fee_type_code=AGFS_FEE'
@@ -31,13 +31,21 @@ class ModifierTypeApiTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data[0], '\'burps\' is not a valid `scenario`')
 
-    def test_get_list_400_with_nonexistent_filter(self):
+    def test_get_list_400_with_nonexistent_offence_class(self):
         response = self.client.get(
             self.endpoint.format(scheme=1) +
             '?offence_class=Z&advocate_type=JRALONE&scenario=2&fee_type_code=AGFS_FEE'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data[0], '\'Z\' is not a valid `offence_class`')
+
+    def test_get_list_400_with_nonexistent_fee_type(self):
+        response = self.client.get(
+            self.endpoint.format(scheme=1) +
+            '?offence_class=A&advocate_type=JRALONE&scenario=2&fee_type_code=AGFS_BURP'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data[0], '\'AGFS_BURP\' is not a valid `fee_type_code`')
 
     def test_get_detail_available(self):
         response = self.client.get(self.endpoint.format(scheme=1) + '1/')
