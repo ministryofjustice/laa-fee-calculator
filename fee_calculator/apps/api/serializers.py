cbdaf7874c87c57e2e12f342fe7be712d76a81ec
Ian Brechin <ian.brechin@gmail.com>
2018-07-02 15:21:08 +0100

    Move API files to API app to better separate functionality

--- fee_calculator/apps/api/serializers.py
+++ fee_calculator/apps/api/serializers.py
@@ -2,7 +2,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
-from .models import (
+from calculator.models import (
     Scheme, Scenario, FeeType, AdvocateType, OffenceClass, Price, Unit,
     ModifierType, Modifier
 )
