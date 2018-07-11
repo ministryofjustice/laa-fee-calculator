a6620e3554fecce93357567bfa12fac10b874985
Ian Brechin <ian.brechin@gmail.com>
2018-07-11 12:17:03 +0100

    Add evidence provision fees for LGFS 2016 and 2017

--- fee_calculator/apps/calculator/tests/test_calculation_lgfs_8.py
+++ fee_calculator/apps/calculator/tests/test_calculation_lgfs_8.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import csv
-from decimal import Decimal, localcontext
+from decimal import Decimal
 import math
 import os
 
@@ -56,6 +56,46 @@ class Lgfs8CalculatorTestCase(CalculatorTestCase):
                 data=data
             )
         )
+
+    def test_evidence_provision_fee_0(self):
+        data = {
+            'scheme': self.scheme_id,
+            'fee_type_code': 'EVID_PROV_FEE',
+            'scenario': 4,
+            'offence_class': 'A',
+            'level': 0
+        }
+        self.check_result(data, Decimal(0))
+
+    def test_evidence_provision_fee_1(self):
+        data = {
+            'scheme': self.scheme_id,
+            'fee_type_code': 'EVID_PROV_FEE',
+            'scenario': 4,
+            'offence_class': 'A',
+            'level': 1
+        }
+        self.check_result(data, Decimal(45))
+
+    def test_evidence_provision_fee_2(self):
+        data = {
+            'scheme': self.scheme_id,
+            'fee_type_code': 'EVID_PROV_FEE',
+            'scenario': 2,
+            'offence_class': 'B',
+            'level': 2
+        }
+        self.check_result(data, Decimal(90))
+
+    def test_evidence_provision_fee_3(self):
+        data = {
+            'scheme': self.scheme_id,
+            'fee_type_code': 'EVID_PROV_FEE',
+            'scenario': 1,
+            'offence_class': 'D',
+            'level': 3
+        }
+        self.check_result(data, Decimal(90))
 
 
 def create_tests():
