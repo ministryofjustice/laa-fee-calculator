0f5e29d67f668512fca0ccabfcc71dddf85e80a3
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2022-04-25 16:17:51 +0100

    Remove old DEFAULT_SCHEMA_CLASS config

--- fee_calculator/settings/base.py
+++ fee_calculator/settings/base.py
@@ -175,7 +175,6 @@ if os.environ.get('SENTRY_DSN'):
 
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
