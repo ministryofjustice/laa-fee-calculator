3e6d00f844ea2c799e6800f528bda875aa436be0
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2022-10-18 13:59:14 +0100

    Add initial CLAIR contingency tests

--- fee_calculator/apps/api/tests/test_scheme_api.py
+++ fee_calculator/apps/api/tests/test_scheme_api.py
@@ -35,6 +35,24 @@ class SchemeApiTestCase(APITestCase):
         self.assertEqual(len(response.data['results']), 1)
         self.assertEqual(response.data['results'][0]['id'], 1)
 
+    def test_get_by_date_available_agfs_12_no_hearing_date(self):
+        response = self.client.get('%s?type=AGFS&case_date=2020-09-17' % self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['id'], 5)  # 5 = AGFS Fee Scheme 12 - CLAR accelerated measures
+
+    def test_get_by_date_available_agfs_12_with_main_hearing_date(self):
+        response = self.client.get('%s?type=AGFS&case_date=2020-09-17&main_hearing_date=2022-09-29' % self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['id'], 5)  # 5 = AGFS Fee Scheme 12 - CLAR accelerated measures
+
+    def test_get_by_date_available_agfs_12a(self):
+        response = self.client.get('%s?type=AGFS&case_date=2020-09-17&main_hearing_date=2022-09-30' % self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['id'], 9)  # 9 = AGFS Fee Scheme 12a - CLAIR contingency
+
     @prevent_request_warnings
     def test_400_on_invalid_date(self):
         response = self.client.get('%s?type=AGFS&case_date=4thJanuary2015' % self.endpoint)
