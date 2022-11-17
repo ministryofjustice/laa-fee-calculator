35e5e98b9140d9b40b8e9dd09aed628526e7fea4
Obsiye <abdirizak.obsiye@digital.justice.gov.uk>
2022-11-17 09:38:55 +0000

    Replace main hearing date ranges with single earliest date
    Replace main_hearing_start_date and main_hearing_end_date range with a single date called earliest_main_hearing_date

--- fee_calculator/apps/viewer/tests/presenters/test_scheme_presenters.py
+++ fee_calculator/apps/viewer/tests/presenters/test_scheme_presenters.py
@@ -39,7 +39,7 @@ class SchemePresenterTestCase(TestCase):
     @classmethod
     def setUp(self):
         self.default_kwargs = {'base_type': 1, 'start_date': '2022-03-05',
-                               'main_hearing_start_date': '2022-03-05', 'main_hearing_end_date': None}
+                               'earliest_main_hearing_date': '2022-03-05'}
         self.presenter_options = {}
 
     def test_base_type_agfs(self):
@@ -58,5 +58,4 @@ class SchemePresenterTestCase(TestCase):
         self.default_kwargs['base_type'] = 1
         scenario = Scheme.objects.create(**self.default_kwargs)
         presenter = SchemePresenter(scenario, **self.presenter_options)
-        self.assertEqual(presenter.main_hearing_start_date, '2022-03-05')
-        self.assertEqual(presenter.main_hearing_end_date, None)
+        self.assertEqual(presenter.earliest_main_hearing_date, '2022-03-05')
