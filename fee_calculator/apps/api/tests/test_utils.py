ffb8a8327d8fceba936ef516e6d340ba27d250cd
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-05-04 11:47:38 +0100

    Change advocate type QC to KC for AGFS Fee Scheme 15

--- fee_calculator/apps/api/tests/test_utils.py
+++ fee_calculator/apps/api/tests/test_utils.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from api.utils import fix_advocate_category
 from rest_framework.test import APIRequestFactory
+from calculator.models import Scheme
 
 
 class FixAdvocateCategoryTestCase(TestCase):
@@ -9,7 +10,7 @@ class FixAdvocateCategoryTestCase(TestCase):
         cls.factory = APIRequestFactory()
 
     def dummy_function(self, return_value):
-        def dummy(request, param_name, required=False, default=None):
+        def dummy(request, param_name, scheme, required=False, default=None):
             return return_value
 
         return dummy
@@ -17,18 +18,35 @@ class FixAdvocateCategoryTestCase(TestCase):
     def test_does_not_alter_QC_advodate_type(self):
         request = self.factory.get('/api/v1/fee-schemes/10/prices/?advocate_type=QC')
         fac = fix_advocate_category(self.dummy_function('QC'))
-        self.assertEqual(fac(request, 'advocate_type'), 'QC')
+        scheme = Scheme.objects.get(pk=10)
+        self.assertEqual(fac(request, 'advocate_type', scheme), 'QC')
 
     def test_changes_KC_advodate_type_to_QC(self):
         request = self.factory.get('/api/v1/fee-schemes/10/prices/?advocate_type=KC')
         fac = fix_advocate_category(self.dummy_function('KC'))
-        self.assertEqual(fac(request, 'advocate_type'), 'QC')
+        scheme = Scheme.objects.get(pk=10)
+        self.assertEqual(fac(request, 'advocate_type', scheme), 'QC')
 
     def test_does_not_alter_a_parameter_other_than_advocate_type(self):
         request = self.factory.get('/api/v1/fee-schemes/10/prices/?other_param=KC')
         fac = fix_advocate_category(self.dummy_function('KC'))
-        self.assertEqual(fac(request, 'other_param'), 'KC')
+        scheme = Scheme.objects.get(pk=10)
+        self.assertEqual(fac(request, 'other_param', scheme), 'KC')
 
-    # TODO:
-    #   For AGFS fee scheme 14 and earlier, the advocate_type needs to be changed to QC
-    #   For AGFS fee scheme 15 and later, the advocate_type needs to be changed to KC
+    def test_does_not_alter_KC_advodate_type_in_AGFS_fee_scheme_15(self):
+        request = self.factory.get('/api/v1/fee-schemes/11/prices/?advocate_type=QC')
+        fac = fix_advocate_category(self.dummy_function('KC'))
+        scheme = Scheme.objects.get(pk=11)
+        self.assertEqual(fac(request, 'advocate_type', scheme), 'KC')
+
+    def test_changes_QC_advodate_type_to_KC_in_AGFS_fee_scheme_15(self):
+        request = self.factory.get('/api/v1/fee-schemes/11/prices/?advocate_type=KC')
+        fac = fix_advocate_category(self.dummy_function('QC'))
+        scheme = Scheme.objects.get(pk=11)
+        self.assertEqual(fac(request, 'advocate_type', scheme), 'KC')
+
+    def test_does_not_alter_a_parameter_other_than_advocate_type_in_AGFS_fee_scheme_15(self):
+        request = self.factory.get('/api/v1/fee-schemes/11/prices/?other_param=QC')
+        fac = fix_advocate_category(self.dummy_function('QC'))
+        scheme = Scheme.objects.get(pk=11)
+        self.assertEqual(fac(request, 'other_param', scheme), 'QC')
--- fee_calculator/apps/api/utils.py
+++ fee_calculator/apps/api/utils.py
@@ -1,13 +1,14 @@
 from functools import wraps
+import datetime
 
 
 def fix_advocate_category(f):
     @wraps(f)
-    def decorator(request, param_name, *args, **kwargs):
-        value = f(request, param_name, *args, **kwargs)
+    def decorator(request, param_name, scheme, *args, **kwargs):
+        value = f(request, param_name, scheme, *args, **kwargs)
 
-        if param_name == 'advocate_type' and value == 'KC':
-            value = 'QC'
+        if param_name == 'advocate_type' and value in ['QC', 'KC']:
+            value = 'KC' if scheme.start_date >= datetime.date(2023, 4, 17) else 'QC'
 
         return value
 
