253f8c49915664c3cdfe1d4076ebb4dfbf93a158
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2026-06-26 17:10:52 +0100

    Add AGFS 17 tests

--- fee_calculator/apps/api/tests/test_scheme_api.py
+++ fee_calculator/apps/api/tests/test_scheme_api.py
@@ -12,6 +12,7 @@ AGFS_SCHEME_TWELVE_ID = 5
 AGFS_CLAIR_CONTINGENCY_SCHEME_ID = 9
 AGFS_SCHEME_FOURTEEN_ID = 10
 LGFS_SCHEME_ELEVEN_ID = 13
+AGFS_SCHEME_SEVENTEEN_ID = 14
 
 
 class SchemeApiTestCase(APITestCase):
@@ -99,3 +100,14 @@ class SchemeApiTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
         self.assertEqual(response.data['results'][0]['id'], LGFS_SCHEME_ELEVEN_ID)
+
+    def test_get_agfs_fee_scheme_17(self):
+        response = self.client.get('%s14/' % self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], AGFS_SCHEME_SEVENTEEN_ID)
+
+    def test_get_by_date_available_agfs_17(self):
+        response = self.client.get('%s?type=AGFS&case_date=2026-06-24' % self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['id'], AGFS_SCHEME_SEVENTEEN_ID)
